b84cd9866d1aa46c85a7b29b4ce6f9833d4e2634
Paul Leonard <paulpaulleonard@gmail.com>
2021-07-23 09:54:12 -0400

    Finish modeling pet

---
+++ models/model_b.rb
@@ -0,0 +1,14 @@
+dog = {
+  name: 'Beau',
+  breed: 'Great Dane',
+  age: 8,
+  weight: 120,
+  is_hungry: false,
+  is_good_boy: true,
+  colors: ['Black', 'Grey', "White"],
+  toys: ['old rug', 'ball', 'other animals']
+}
+
+p dog
+
+#Yes he is real and yes his favorite toy is an old rug lol
