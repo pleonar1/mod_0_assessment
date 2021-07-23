de56e76321ee05ab2c466a618d8e5e92dbd1ef18
Paul Leonard <paulpaulleonard@gmail.com>
2021-07-23 09:37:10 -0400

    Complete program specific challenge

---
+++ variables.rb
@@ -0,0 +1,19 @@
+famous_person = "Tom Hanks"
+
+is_active = true
+
+famous_person = "Bill Murray"
+
+num_3 = 3
+
+num_9 = 9
+
+sum = num_3 + num_9
+
+puts sum #prints it for proof
+
+animals = ['Lions', 'Tigers', 'Bears']
+animals.shift
+puts animals #prints for proof
+
+puts famous_person.size
