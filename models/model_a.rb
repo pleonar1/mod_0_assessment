18dc12747bac60961dbc15673f9ee82a5a8ffa95
Paul Leonard <paulpaulleonard@gmail.com>
2021-07-23 09:47:36 -0400

    Finish modeling school

---
+++ models/model_a.rb
@@ -0,0 +1,10 @@
+uga = {
+  mascot: "Bulldog",
+  colors: "Red and Black",
+  tuition: 17000,
+  championship: 1972,
+  is_fun: true,
+  is_challenging: true,
+  majors: ['Business', 'Agriculture', 'Psychology'],
+  minors: ['English', 'Poultry Science', 'Sports Science']
+}
