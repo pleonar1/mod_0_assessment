4d439b5fc35b62ca1b0cc7b1d1726832dd38a5a2
Paul Leonard <paulpaulleonard@gmail.com>
2021-07-26 12:41:03 -0400

    Add models

---
+++ models/model_airport.rb
@@ -0,0 +1,10 @@
+atl_airport = {
+  location: "Atlanta, Georgia",
+  name: "Heartsfield Jackson International Airport",
+  number_of_terminals: 100,
+  number_of_restaraunts: 200,
+  has_international_flights: true,
+  has_overnight_parking: true,
+  int_carriers: ["Brittish Airways", "Malaysian Airlines", "Swiss"],
+  dom_carriers: ['Delta', 'American Airlines', 'Southwest Airlines,', 'Spirit']
+}
---
+++ models/model_vehicle.rb
@@ -0,0 +1,10 @@
+sequoia = {
+  manufacturer: "Toyota",
+  color: "Blue",
+  year: 2010,
+  mpg: 14.3,
+  is_suv: true,
+  is_spacious: true,
+  previous_owners: ['Stephanie', 'Stacie', 'James'],
+  features: ['CD player', 'Dvd player', 'Sport Mode', 'Sunroof']
+}
